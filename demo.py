d8fe4e5133cd7462e68438a986519958b54e4818
areenkumarsahu-afk <areenkumarsahu@gmail.com>
2025-09-24 10:52:40 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from PIL import Image
 import io
 
 img=Image.open("brand.avif")
-img=img.resize((175,175))
+img=img.resize((125,125))
 buf=io.BytesIO()
 img.save(buf,format="JPEG")
 buf.seek(0)
@@ -57,6 +57,7 @@ else:
         except Exception:
 
             st.error("Request failed.")
+
 
 
 
